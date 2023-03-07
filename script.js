834af190339f30f216fadff75614e82424ad2bfd
Letmediee <114399580+Letmediee@users.noreply.github.com>
2023-03-07 13:59:03 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -13,4 +13,10 @@ function jump() {
     }, 300)
 }
 
-
+let isAlive = setInterval (function () {
+    let dino = parseInt(window.getComputedStyle(character).getPropertyValue('top'))
+    let cactus = parseInt(window.getComputedStyle(block).getPropertyValue('left'))
+    if (cactus < 50 && cactus > 0 && dino >= 140){
+    alert('GAME OVER')
+}
+}, 10)
